4f95708350a59b4d8b4234d8d5fe0ff2b136b4a0
svdunaev <54572104+svdunaev@users.noreply.github.com>
2020-04-06 17:16:17 +0500

    настраивает установленный сервер

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public'),
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+  },
   devtool: 'source-map',
 };
